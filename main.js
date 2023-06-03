16065dbc2d32dd4ec49681b4b4f81b115a45bc9b
agsinha2009 <101357307+agsinha2009@users.noreply.github.com>
2023-06-03 16:15:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.position(700,200);
+    video=createCapture(VIDEO);
+    video.size(400,400)
+    video.position(200,230);
+}
+
+function draw(){
+    background('#00000');
+}
